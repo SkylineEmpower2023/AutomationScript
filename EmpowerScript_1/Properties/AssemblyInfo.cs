c0045618e4e575ff9a9a7331154c628b47c2f151
manas.chakraborty <manas.chakraborty@LAPTOP-OHGCE85T>
2023-03-28 14:13:51 +0200

    Add project files.

---
+++ EmpowerScript_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerScript_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerScript_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("C3E27A4D-6F57-4ED7-8636-01C2637605D8")]
+[assembly: AssemblyVersion("1.0.0.0")]
